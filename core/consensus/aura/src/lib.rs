d6d709bd302ed469d000f54ed6324d624ae4b81c
Robert Habermeier <rphmeier@gmail.com>
2018-10-16 14:22:43 -0700

    get aura compiling and add to workspace

--- core/consensus/aura/src/lib.rs
+++ core/consensus/aura/src/lib.rs
@@ -14,14 +14,20 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Aura consensus algorithm, implemented for substrate.
+//! Aura (Authority-round) consensus in substrate.
 //!
-//! This works with a list of authorities A and monotonic time clock.
-//! Time is divided into discrete steps. For each time step T, an authority
-//! is chosen from the list as `A[T % len(A)]` and has the chance to author
-//! a block.
+//! Aura works by having a list of authorities A who are expected to roughly
+//! agree on the current time. Time is divided up into discrete slots of t
+//! seconds each. For each slot s, the author of that slot is A[s % |A|].
+//!
+//! The author is allowed to issue one block but not more during that slot,
+//! and it will be built upon the longest valid chain that has been seen.
+//!
+//! Blocks from future steps will be either deferred or rejected depending on how
+//! far in the future they are.
 
 extern crate parity_codec as codec;
+extern crate substrate_consensus_common as consensus_common;
 extern crate substrate_client as client;
 extern crate substrate_primitives as primitives;
 extern crate srml_support as runtime_support;
@@ -52,6 +58,7 @@ use std::sync::Arc;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::time::{Instant, Duration};
 
+use consensus_common::{Authorities, BlockImport, Environment, Proposer};
 use client::{ChainHead, ImportBlock, BlockOrigin};
 use codec::Encode;
 use runtime_primitives::{generic::BlockId, Justification};
@@ -59,21 +66,8 @@ use runtime_primitives::traits::{Block, Header};
 use primitives::{AuthorityId, ed25519, ed25519::LocalizedSignature};
 
 use futures::{Async, Stream, Sink, Future, IntoFuture};
-use futures::sync::oneshot;
 use tokio::timer::Interval;
 use parking_lot::Mutex;
-
-//! Aura (Authority-round) consensus in substrate.
-//!
-//! Aura works by having a list of authorities A who are expected to roughly
-//! agree on the current time. Time is divided up into discrete slots of t
-//! seconds each. For each slot s, the author of that slot is A[s % |A|].
-//!
-//! The author is allowed to issue one block but not more during that slot,
-//! and it will be built upon the longest valid chain that has been seen.
-//!
-//! Blocks from future steps will be either deferred or rejected depending on how
-//! far in the future they are.
 
 /// Configuration for Aura consensus.
 pub struct Config {
@@ -92,7 +86,11 @@ fn slot_author<B: Block, C: Authorities<B>>(slot_num: u64, client: &C, header: &
 		Ok(authorities) => {
 			if authorities.is_empty() { return Ok(None) }
 
-			let current_author = *authorities.get(slot_num % authorities.len())
+			let idx = slot_num % (authorities.len() as u64);
+			assert!(idx <= usize::max_value() as u64,
+				"It is impossible to have a vector with length beyond the address space; qed");
+
+			let current_author = *authorities.get(idx as usize)
 				.expect("authorities not empty; index constrained to list length;\
 					 this is a valid index; qed");
 
@@ -109,10 +107,10 @@ fn slot_now(slot_duration: u64) -> Result<u64, ()> {
 	match now.duration_since(SystemTime::UNIX_EPOCH) {
 		Ok(dur) => Ok(dur.as_secs() / slot_duration),
 		Err(_) => {
-			warn!("Current time {:?} is before unix epoch. Something is wrong." now);
+			warn!("Current time {:?} is before unix epoch. Something is wrong.", now);
 			return Err(())
 		}
-	};
+	}
 }
 
 /// Start the aura worker. This should be run in a tokio runtime.
@@ -121,71 +119,79 @@ pub fn start_aura<B, C, E, Error>(config: Config, client: Arc<C>, env: Arc<E>)
 	B: Block,
 	C: Authorities<B, Error=Error> + BlockImport<B, Error=Error> + ChainHead<B>,
 	E: Environment<B, Error=Error>,
-	P: Proposer<B, Error=Error>,
+	E::Proposer: Proposer<B, Error=Error>,
+	Error: ::std::error::Error + Send + 'static + From<::consensus_common::Error>,
 {
 	use futures::future::Either;
 
 	let local_keys = config.local_key.map(|pair| (pair.public(), pair));
 	let slot_duration = config.slot_duration;
+	let mut last_authored_slot = 0;
+
 	Interval::new_interval(Duration::from_secs(slot_duration))
 		.filter_map(move |_| local_keys.clone()) // skip if not authoring.
-		.map_err(consensus_common::ErrorKind::FaultyTimer)
-		.map_err(consensus_common::Error::from)
-		.map_err(Error::from)
+		.map_err(|e|  debug!(target: "aura", "Faulty timer: {:?}", e))
 		.for_each(move |(public_key, key)| {
+			use futures::future;
+
 			let slot_num = match slot_now(slot_duration) {
 				Ok(n) => n,
-				Err(()) => return Either::B(Err(())),
+				Err(()) => return Either::B(future::err(())),
 			};
+
+			if last_authored_slot >= slot_num { return Either::B(future::ok(())) }
+			last_authored_slot = slot_num;
 
 			let chain_head = match client.best_block_header() {
 				Ok(x) => x,
 				Err(e) => {
 					warn!("Unable to author block in slot {}. no best block header: {:?}", slot_num, e);
-					return Either::B(Ok(()))
+					return Either::B(future::ok(()))
 				}
 			};
 
 			let proposal_work = match slot_author(slot_num, &*client, &chain_head) {
-				Ok(None) => return Either::B(Ok(())),
+				Ok(None) => return Either::B(future::ok(())),
 				Ok(Some((author, authorities))) => if author.0 == public_key.0 {
 					// we are the slot author. make a block and sign it.
 					let proposer = match env.init(&chain_head, &authorities, key.clone()) {
 						Ok(p) => p,
 						Err(e) => {
 							warn!("Unable to author block in slot {:?}: {:?}", slot_num, e);
-							return Either::B(Ok(()))
+							return Either::B(future::ok(()))
 						}
 					};
 
-					proposer.propose()
+					proposer.propose().into_future()
 				} else {
-					return Either::B(Ok(()));
+					return Either::B(future::ok(()));
 				}
 				Err(e) => {
 					warn!("Unable to fetch authorities at block {:?}: {:?}", chain_head.hash(), e);
-					return Either::B(Ok(()));
+					return Either::B(future::ok(()));
 				}
 			};
 
 			let block_import = client.clone();
-			proposal_work
+			Either::A(proposal_work
 				.map(move |b| {
 					// TODO: add digest item and import
+					let (header, body) = b.deconstruct();
+					let hash = header.hash();
 					let import_block = ImportBlock {
 						origin: BlockOrigin::Own,
-						header: b.header,
+						header,
 						external_justification: Vec::new(),
 						internal_justification: Vec::new(), // TODO
-						body: b.extrinsics,
+						body: Some(body),
 						finalized: false,
 						auxiliary: Vec::new(),
 					};
-					let hash = b.header.hash();
 					if let Err(e) = block_import.import_block(import_block, None) {
 						warn!("Error importing block {:?}", hash);
 					}
 				})
-				.map_err(|e| warn!("Failed to construct block: {:?}", e));
+				.map_err(|e| warn!("Failed to construct block: {:?}", e))
+			)
 		})
 }
